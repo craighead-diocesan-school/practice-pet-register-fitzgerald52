b735cad044800e29b705cd8048eab26b912a11c0
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2023-07-25 10:06:35 +1200

    i made a button that adds a name to the array and another that shows all the names

---
+++ script.js
@@ -0,0 +1,16 @@
+let nameArray = ['Max ', 'Bella ', 'Cooper ', 'Milo ', 'Buddy ']
+
+function nameadding() {
+  //lets user input in name of dog
+  let name = prompt('what is the name of the new dog?')
+  //add a new name to array of names
+  nameArray.push(name)
+}
+
+function removename()
+
+function names() {
+  // shows names of all pets
+  alert(nameArray)
+}
+
